80f559d675f66172b2407fdabafd1e0fd9991a38
Daniel Lee <DanielKJLee@DanielKngjaeLee.wireless.yale.internal>
2018-04-02 02:16:07 -0400

    css typeface live

---
+++ projects/css-typeface/giving-tree.js
@@ -0,0 +1,83 @@
+$(document).ready(function(){
+
+  $('.button').click(function(){
+    $('#a .top').css("border-top-width", 228);
+    $('#a .bottom').css("border-bottom-width", 228);
+
+    $('#b .top').css("border-top-width", 67);
+    $('#b .bottom').css("border-bottom-width", 67);
+
+    $('#c .top').css("border-top-width", 51);
+    $('#c .bottom').css("border-bottom-width", 51);
+
+    $('#d .top').css("border-top-width", 123);
+    $('#d .bottom').css("border-bottom-width", 123);
+
+    $('#e .top').css("border-top-width", 269);
+    $('#e .bottom').css("border-bottom-width", 269);
+
+    $('#f .top').css("border-top-width", 23);
+    $('#f .bottom').css("border-bottom-width", 23);
+
+    $('#g .top').css("border-top-width", 32);
+    $('#g .bottom').css("border-bottom-width", 32);
+
+    $('#h .top').css("border-top-width", 133);
+    $('#h .bottom').css("border-bottom-width", 133);
+
+    $('#i .top').css("border-top-width", 111);
+    $('#i .bottom').css("border-bottom-width", 111);
+
+    $('#j .top').css("border-top-width", 3);
+    $('#j .bottom').css("border-bottom-width", 3);
+
+    $('#k .top').css("border-top-width", 19);
+    $('#k .bottom').css("border-bottom-width", 19);
+
+    $('#l .top').css("border-top-width", 72);
+    $('#l .bottom').css("border-bottom-width", 72);
+
+    $('#m .top').css("border-top-width", 75);
+    $('#m .bottom').css("border-bottom-width", 75);
+
+    $('#n .top').css("border-top-width", 144);
+    $('#n .bottom').css("border-bottom-width", 144);
+
+    $('#o .top').css("border-top-width", 155);
+    $('#o .bottom').css("border-bottom-width", 155);
+
+    $('#p .top').css("border-top-width", 54);
+    $('#p .bottom').css("border-bottom-width", 54);
+
+    $('#q .top').css("border-top-width", 1);
+    $('#q .bottom').css("border-bottom-width", 1);
+
+    $('#r .top').css("border-top-width", 94);
+    $('#r .bottom').css("border-bottom-width", 94);
+
+    $('#s .top').css("border-top-width", 112);
+    $('#s .bottom').css("border-bottom-width", 112);
+
+    $('#t .top').css("border-top-width", 179);
+    $('#t .bottom').css("border-bottom-width", 179);
+
+    $('#u .top').css("border-top-width", 62);
+    $('#u .bottom').css("border-bottom-width", 62);
+
+    $('#v .top').css("border-top-width", 19);
+    $('#v .bottom').css("border-bottom-width", 19);
+
+    $('#w .top').css("border-top-width", 55);
+    $('#w .bottom').css("border-bottom-width", 55);
+
+    $('#x .top').css("border-top-width", 0);
+    $('#x .bottom').css("border-bottom-width", 0);
+
+    $('#y .top').css("border-top-width", 100);
+    $('#y .bottom').css("border-bottom-width", 100);
+
+    $('#z .top').css("border-top-width", 0);
+    $('#z .bottom').css("border-bottom-width", 0);
+  })
+
+})
